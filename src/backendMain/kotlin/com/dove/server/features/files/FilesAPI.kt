48294fad0ddc5c2b59814db44f958e6ca37337e6
vadim <justneon33@gmail.com>
2021-09-07 00:35:47 +0300

    chore: api updated in order to optimization

--- src/backendMain/kotlin/com/dove/server/features/files/FilesAPI.kt
+++ src/backendMain/kotlin/com/dove/server/features/files/FilesAPI.kt
@@ -8,12 +8,16 @@ import com.dove.data.users.tokens.TokenType
 import com.dove.server.features.users.tokens.TokensStorage
 import com.dove.server.local.Environment
 import com.dove.server.utils.hashing.toMD5
+import com.dove.server.utils.random.nextString
+import com.papsign.ktor.openapigen.content.type.multipart.NamedFileInputStream
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.InputStream
 import java.util.*
 import kotlin.io.path.createFile
-import kotlin.io.path.readBytes
-import kotlin.io.path.writeBytes
+import kotlin.io.path.inputStream
+import kotlin.io.path.outputStream
+import kotlin.random.Random
 
 object FilesAPI {
     /**
@@ -22,16 +26,17 @@ object FilesAPI {
      * @return [String] representation of [java.util.UUID].
      */
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun upload(token: String, realName: String, bytes: ByteArray): ApiResult<String> {
+    suspend fun upload(token: String, realName: String, stream: NamedFileInputStream): ApiResult<String> {
         val auth = TokensStorage.read(token) ?: return Either.error(InvalidTokenError())
         return if (auth.type != TokenType.REGULAR)
             Either.error(InvalidTokenError())
         else {
-            val fileHash = bytes.toMD5()
+            val fileName = Random.nextString(32)
+            val fileHash = stream.toMD5()
             withContext(Dispatchers.IO) {
-                Environment.files.resolve(fileHash).apply {
+                Environment.files.resolve(fileName).apply {
                     createFile()
-                    writeBytes(bytes)
+                    stream.transferTo(outputStream())
                 }
             }
             Either.success(FilesStorage.create(realName, fileHash, fileHash, auth.userId))
@@ -42,10 +47,10 @@ object FilesAPI {
      * Reads file bytes by [fileUUID].
      */
     @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun getFileBytes(fileUUID: String): ApiResult<ByteArray> {
+    suspend fun getFileBytes(fileUUID: String): ApiResult<InputStream> {
         val file = FilesStorage.read(UUID.fromString(fileUUID)) ?: return Either.error(FileNotFoundError)
         return Either.success(withContext(Dispatchers.IO) {
-            Environment.files.resolve(file.fileHash).readBytes()
+            Environment.files.resolve(file.fileHash).inputStream()
         })
     }
 }
