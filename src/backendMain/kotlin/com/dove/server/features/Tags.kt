c841ee50ff62239ae6cbe5fe986603ea8d818b8e
vadim <justneon33@gmail.com>
2021-09-07 00:39:08 +0300

    feat: new Tag

--- src/backendMain/kotlin/com/dove/server/features/Tags.kt
+++ src/backendMain/kotlin/com/dove/server/features/Tags.kt
@@ -3,8 +3,10 @@ package com.dove.server.features
 import com.papsign.ktor.openapigen.APITag
 
 enum class Tags(override val description: String) : APITag {
-    Users("Users API"), Tokens("Tokens API"),
+    Users("Users API"),
+    Tokens("Tokens API"),
     Chats("Chats API"),
     Messages("Messages API"),
-    Members("Members API")
+    Members("Members API"),
+    Files("Files API")
 }
