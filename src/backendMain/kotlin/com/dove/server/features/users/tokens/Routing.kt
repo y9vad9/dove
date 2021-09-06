7e05bd3fd0cc941cdea514484e4cf43b5ceb016b
vadim <justneon33@gmail.com>
2021-09-07 00:39:19 +0300

    feat: new routes

--- src/backendMain/kotlin/com/dove/server/features/users/tokens/Routing.kt
+++ src/backendMain/kotlin/com/dove/server/features/users/tokens/Routing.kt
@@ -1,17 +1,20 @@
 package com.dove.server.features.users.tokens
 
-import com.dove.data.monad.onError
-import com.dove.data.monad.onSuccess
+import com.dove.data.users.tokens.Token
 import com.dove.server.features.Tags
-import com.dove.server.utils.openapi.respondError
+import com.dove.server.utils.openapi.delete
+import com.dove.server.utils.openapi.get
+import com.papsign.ktor.openapigen.annotations.parameters.HeaderParam
 import com.papsign.ktor.openapigen.annotations.parameters.QueryParam
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
-import com.papsign.ktor.openapigen.route.path.normal.get
-import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.tag
 
 fun NormalOpenAPIRoute.tokens() = tag(Tags.Tokens) {
     createToken()
+    getTokenRequest()
+    getTokensRequest()
+    unauthorizeMe()
+    unauthorize()
 }
 
 private data class CreateTokenRequest(
@@ -20,11 +23,37 @@ private data class CreateTokenRequest(
 )
 
 private fun NormalOpenAPIRoute.createToken() {
-    get<CreateTokenRequest, Unit> { parameters ->
-        TokensAPI.create(parameters.email).onSuccess {
-            respond(it)
-        }.onError {
-            respondError(it)
-        }
+    get<CreateTokenRequest, Unit> {
+        TokensAPI.create(email)
     }
 }
+
+data class AuthorizeByTokenRequest(
+    @HeaderParam("Authorization token")
+    val token: String
+)
+
+private fun NormalOpenAPIRoute.getTokenRequest() = get<AuthorizeByTokenRequest, Token> {
+    TokensAPI.getToken(token)
+}
+
+private fun NormalOpenAPIRoute.getTokensRequest() = get<AuthorizeByTokenRequest, List<Token>> {
+    TokensAPI.getTokens(token)
+}
+
+private fun NormalOpenAPIRoute.unauthorizeMe() = delete<AuthorizeByTokenRequest, Unit> {
+    TokensAPI.unauthorize(token)
+}
+
+private data class UnauthorizeRequest(
+    @HeaderParam("Authorization token")
+    val token: String,
+    @QueryParam("Token to unauthorize.")
+    val unauthTokenId: Long
+)
+
+private fun NormalOpenAPIRoute.unauthorize() = delete<UnauthorizeRequest, Unit> {
+    TokensAPI.unauthorize(token, unauthTokenId)
+}
+
+
